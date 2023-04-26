efb0ee79250e8572cbfd4e60f79db21e5827cd87
Nico Yeldynov <yeldynov@gmail.com>
2023-04-26 13:19:23 +0300

    initialized react navigation v4

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
