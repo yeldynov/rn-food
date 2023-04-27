5ce6dc8eaf3eac2f618a3aadbbac3d7ec101d039
Nico Yeldynov <yeldynov@gmail.com>
2023-04-27 15:25:04 +0300

    updated navigation to react navigation v6

---
+++ src/components/ResultsList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import ResultsDetail from './ResultsDetail';
+
+const ResultsList = ({ title, results }) => {
+  const { navigate } = useNavigation();
+
+  if (!results.length) return;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigate('ResultsShow', { id: item.id })}
+            >
+              <ResultsDetail result={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
+  },
+});
+
+export default ResultsList;
